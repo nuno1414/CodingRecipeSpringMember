747de32a42029315839a0a54b98e53c60ba632c4
nuno1414 <n_eskim@naver.com>
2024-02-28 17:16:57 +0900

    Coding Recipe Spring Framework - Member Project Source

--- workspace/member_framework/src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ workspace/member_framework/src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -2,12 +2,14 @@ package com.codingrecipe.member.controller;
 
 import com.codingrecipe.member.dto.MemberDTO;
 import com.codingrecipe.member.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/member")
@@ -34,4 +36,87 @@ public class MemberController {
         }
     }
 
+    @GetMapping("/login")
+    public String loginForm(){
+
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDTO memberDTO,
+                        HttpSession session) {
+
+        boolean loginResult = memberService.login(memberDTO);
+
+        if (loginResult) {
+            session.setAttribute("loginEmail", memberDTO.getMemberEmail());
+            return "main";
+        } else {
+            return "login";
+        }
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model){
+
+        List<MemberDTO> memberDTOList = memberService.findAll();
+
+        model.addAttribute("memberList", memberDTOList);
+
+        return "list";
+    }
+
+    @GetMapping // /member?id=1
+    public String findById(@RequestParam("id") Long id, Model model){ // Model로 view에 데이터 넘겨줌.
+
+        MemberDTO memberDTO = memberService.findById(id);
+
+        model.addAttribute("member", memberDTO);
+
+        return "detail";
+
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") Long id){
+
+        memberService.delete(id);
+
+        return "redirect:/member/"; //redirect 뒤에는 주소값(url 호출) 값이 와야 함.
+    }
+
+    @GetMapping("/update")
+    public String updateForm(HttpSession session, Model model){
+        //세션에 저장된 나의 이메일 가져오기.
+        String loginEmail = (String)session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.findByMemberEmail(loginEmail);
+
+        model.addAttribute("member", memberDTO);
+
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute MemberDTO memberDTO){
+
+        boolean updateResult = memberService.update(memberDTO);
+
+        if(updateResult) {
+            return "redirect:/member?id="+memberDTO.getId();
+        } else {
+            return "index";
+        }
+    }
+
+    @PostMapping("/email-check")
+    //json 형식으로 응답 시에는 @ResponseBody나 @ResponseEntity를 사용함
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+
+        System.out.println("memberEmail = " + memberEmail);
+
+        String checkResult = memberService.emailCheck(memberEmail);
+
+        return checkResult;
+
+    }
 }
--- workspace/member_framework/src/main/java/com/codingrecipe/member/repository/MemberRepository.java
+++ workspace/member_framework/src/main/java/com/codingrecipe/member/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
@@ -15,5 +17,35 @@ public class MemberRepository {
         System.out.println("memberDTO = " + memberDTO);
 
         return sql.insert("Member.save", memberDTO);
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+
+        return sql.selectOne("Member.login", memberDTO);
+    }
+
+    public List<MemberDTO> findAll() {
+
+        return sql.selectList("Member.findAll");
+    }
+
+    public MemberDTO findById(Long id) {
+
+        return sql.selectOne("Member.findById", id);
+    }
+
+    public void delete(Long id) {
+
+        sql.delete("Member.delete", id);
+    }
+
+    public MemberDTO findByMemberEmail(String loginEmail) {
+
+        return sql.selectOne("Member.findByMemberEmail", loginEmail);
+    }
+
+    public int update(MemberDTO memberDTO) {
+
+        return sql.update("Member.update", memberDTO);
     }
 }
